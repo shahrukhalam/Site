fd97ffa622b53372a02692dac00f1e2ffee9184e
Shahrukh Alam <alam.shahrukh786@gmail.com>
2023-01-26 22:33:35 +0530

    Adds apple touch icon

--- Sources/Pages/Articles/CSS/CommonCSSLinks.swift
+++ Sources/Pages/Articles/CSS/CommonCSSLinks.swift
@@ -3,5 +3,6 @@ import HTMLDSL
 var commonCSSLinks: some HTMLContentView {
     AnyView([
         CSSLink(favicon: "images/index/favicon.png"),
+        CSSLink(appleTouchIcon: "images/index/apple-touch-icon.png"),
     ])
 }
