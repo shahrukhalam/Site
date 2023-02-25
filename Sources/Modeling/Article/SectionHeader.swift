32664d387f5dd8f45e0fc92842d859b01f6645db
Shahrukh <shahrukh@swapcard.com>
2023-02-25 10:38:29 +0530

    preload first image

--- Sources/Modeling/Article/SectionHeader.swift
+++ Sources/Modeling/Article/SectionHeader.swift
@@ -1,9 +1,9 @@
 public struct SectionHeader {
     public let description: Description
     public let hero: Detail
-    public let grids: [(Detail, isHiddenInDesktop: Bool)]
+    public let grids: [(detail: Detail, isHiddenInDesktop: Bool)]
 
-    public init(description: Description, hero: Detail, grids: [(Detail, isHiddenInDesktop: Bool)]) {
+    public init(description: Description, hero: Detail, grids: [(detail: Detail, isHiddenInDesktop: Bool)]) {
         self.description = description
         self.hero = hero
         self.grids = grids
