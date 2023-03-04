7931be9662a16b3a9c317f6dbc1ecbd1e63b0713
Shahrukh <shahrukh@swapcard.com>
2023-03-04 21:07:17 +0530

    [v 0.0.8] corrects article route from articles list page

--- Sources/Pages/Articles/Pages/IndexPage.swift
+++ Sources/Pages/Articles/Pages/IndexPage.swift
@@ -11,6 +11,7 @@ public func indexPage(tabs: [LinkDescription], selectedIndex: Int, header: Secti
                 indexPageCSS
 
                 AnalyticsScript(id: analyticsID)
+
 //                Script(url: "https://unpkg.com/vue@3/dist/vue.global.prod.js")
 //                CSSLink(preloadImage: header.grids.first?.detail.image.url ?? "")
             }
@@ -18,6 +19,7 @@ public func indexPage(tabs: [LinkDescription], selectedIndex: Int, header: Secti
             Body {
                 NavView(tabs: tabs, selectedIndex: selectedIndex)
                 sectionHeader(with: header)
+
 //                Div("").identifyBy(id: "app")
 //                AnalyticsScript(id: analyticsID)
             }
