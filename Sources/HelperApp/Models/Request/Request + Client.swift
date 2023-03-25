579f24d320ffc2b69cf290f31fb502db9818ba03
Shahrukh Alam <alam.shahrukh786@gmail.com>
2023-03-25 23:42:13 +0530

    brings back correct check for app

--- Sources/HelperApp/Models/Request/Request + Client.swift
+++ Sources/HelperApp/Models/Request/Request + Client.swift
@@ -1,8 +1,14 @@
 import Vapor
 
 public extension Request {
+    /// This is important as Vapor implements `Equatable` in a weird way considering `any`(all) type
+    /// That's why we rely on `description`
     var isApp: Bool {
-        let acceptsOnlyJSON = headers.accept.mediaTypes == [.json]
+        let acceptsOnlyJSON = headers.accept.mediaTypes.description == [HTTPMediaType.jsonApp].description
         return acceptsOnlyJSON
     }
+}
+
+private extension HTTPMediaType {
+    static let jsonApp = HTTPMediaType(type: "application", subType: "json")
 }
