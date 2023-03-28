51dbec9863e499791632e016ad6a46c8ad5fe5a2
Shahrukh Alam <alam.shahrukh786@gmail.com>
2023-03-28 20:48:23 +0530

    [WIP] Handles framework route

--- Sources/Pages/Articles/CSS/CommonCSSLinks.swift
+++ Sources/Pages/Articles/CSS/CommonCSSLinks.swift
@@ -2,8 +2,8 @@ import HTMLDSL
 
 var commonCSSLinks: some HTMLContentView {
     AnyView([
-        CSSLink(favicon: "images/index/favicon.png"),
-        CSSLink(appleTouchIcon: "images/index/apple-touch-icon.png"),
+        CSSLink(favicon: "/images/index/favicon.png"),
+        CSSLink(appleTouchIcon: "/images/index/apple-touch-icon.png"),
         CSSLink(path: "https://fonts.googleapis.com/css?family=Poppins:wght@200..900&display=swap")
     ])
 }
