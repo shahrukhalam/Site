c41c3757f2e443a39ae4b6b7814e631b063773fd
Shahrukh Alam <alam.shahrukh786@gmail.com>
2023-07-17 10:25:52 +0530

    fixes share layout

--- Sources/Pages/Articles/Views/ArticleView.swift
+++ Sources/Pages/Articles/Views/ArticleView.swift
@@ -38,9 +38,10 @@ var shareStyle: CSSStyle {
     let pStyle = ClassStyle(forClass: .share, withTag: .enclosing(.paragraph))
         .font(size: .relativeToRootFontSize(Typography.Font.Size.body))
         .lineHeight(.number(Typography.LineHeight.body))
-        .margin(bottom: .length(.relativeToRoot(Typography.Margin.body)))
 
     let linkStyle = ClassStyle(forClass: .share, withTag: .enclosing(.link))
+        .display(.inlineFlex)
+        .flexDistribute(.center)
         .foregroundColor(isDarkMode ? Color.Dark.LinkNormalForeground : Color.Light.LinkNormalForeground)
         .textDecoration(.none)
     let linkHoverStyle = ClassStyle(.share, tag: .enclosing(.link), cssTag: .hover)
@@ -57,24 +58,18 @@ private func share(title: String, url: String) -> some HTMLBodyContentView {
         Div {
             ImageLink(url: "https://twitter.com/intent/tweet?text=I recommend \(title) by Shahrukh Alam \(url)", content: {
                 Image("/images/articles/twitter.png", alternateText: "twitter icon")
-                // TODO: This is a hack, make it work with `flex` + `inlineBlock`
-                    .margin(bottom: .pixel(-2))
                 Paragraphs("Share this article on Twitter")
-                    .display(.inline)
+                    .padding(left: .length(.relativeToRoot(Typography.Margin.body)))
             })
-            .display(.inlineBlock)
             .target(.newWindowOrTab)
         }
 
         Div {
             ImageLink(url: "https://www.linkedin.com/sharing/share-offsite/?url=\(url)", content: {
                 Image("/images/articles/linkedin.png", alternateText: "linkedin icon")
-                // TODO: This is a hack, make it work with `flex` + `inlineBlock`
-                    .margin(bottom: .pixel(-2))
                 Paragraphs("Share this article on Linkedin")
-                    .display(.inline)
+                    .padding(left: .length(.relativeToRoot(Typography.Margin.body)))
             })
-            .display(.inlineBlock)
             .target(.newWindowOrTab)
         }
     }
