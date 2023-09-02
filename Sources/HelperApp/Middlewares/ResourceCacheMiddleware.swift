acf291633c9ef16a97e5a697abaf33e1689a603a
Shahrukh Alam <alam.shahrukh786@gmail.com>
2023-09-02 19:52:10 +0530

    adds what can't be cached

--- Sources/HelperApp/Middlewares/ResourceCacheMiddleware.swift
+++ Sources/HelperApp/Middlewares/ResourceCacheMiddleware.swift
@@ -2,16 +2,38 @@ import Vapor
 
 public struct ResourceCacheMiddleware: AsyncMiddleware {
     public init() {  }
-
+    
     public func respond(to request: Request, chainingTo next: AsyncResponder) async throws -> Response {
         let response = try await next.respond(to: request)
         
         try! request.application.liveRun {
-            if !request.relativeURL.contains("rss.xml") {
+            if request.isCacheable {
                 response.headers.cacheControl = .init(isPublic: true, maxAge: 604800) // A week
             }
         }
-
+        
         return response
+    }
+}
+
+private extension Request {
+    var nonCacheables: [String] {
+        [
+            "rss",
+            "robots",
+            "sitemaps/"
+        ]
+    }
+    
+    var isCacheable: Bool {
+        var isCacheable: Bool = true
+        for nonCacheable in nonCacheables {
+            if relativeURL.contains(nonCacheable) {
+                isCacheable = false
+                break
+            }
+        }
+        
+        return isCacheable
     }
 }
