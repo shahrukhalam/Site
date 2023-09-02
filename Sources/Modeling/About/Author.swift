11315b2e50df986be8d9756ddecce1f0364d5e81
Shahrukh Alam <alam.shahrukh786@gmail.com>
2023-09-02 20:52:15 +0530

    Adds OptionSet for Author Role

--- Sources/Modeling/About/Author.swift
+++ Sources/Modeling/About/Author.swift
@@ -1,15 +1,40 @@
 public struct Author {
     public struct Detail {
         public let name: String
-        public let role: String
-        public let location: String
+        public let role: Role
 
-        public init(name: String, role: String, location: String) {
+        public init(name: String, role: Role) {
             self.name = name
             self.role = role
-            self.location = location
+        }
+        
+        public struct Role: OptionSet {
+            public let rawValue: Int
+
+            public init(rawValue: Int) {
+                self.rawValue = rawValue
+            }
+                        
+            public static let coauthor: Role = .init(rawValue: 1 << 0)
+            public static let creator: Role = .init(rawValue: 1 << 1)
+            public static let all: Role = [creator, .coauthor]
         }
     }
 
     public let detail: Detail
+}
+
+extension Author.Detail.Role: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .creator:
+            return "creator"
+        case .coauthor:
+            return "coauthor"
+        case .all:
+            return "creator & coauthor"
+        default:
+            fatalError("Unexpected Role at this point")
+        }
+    }
 }
