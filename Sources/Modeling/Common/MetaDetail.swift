e01a832dca0a01ac5dd560cb0cea971727f7a137
Shahrukh Alam <alam.shahrukh786@gmail.com>
2023-09-02 20:01:24 +0530

    gets rid of Meta keywords

--- Sources/Modeling/Common/MetaDetail.swift
+++ Sources/Modeling/Common/MetaDetail.swift
@@ -1,15 +1,13 @@
 public struct MetaDetail {
     public let title: String
     public let description: String
-    public let keywords: Set<String>
     public let absoluteSocialImage: String
     public let absoluteURL: String
     public let canonicalURL: String
 
-    public init(title: String, description: String, keywords: Set<String>, absoluteSocialImage: String, absoluteURL: String, canonicalURL: String) {
+    public init(title: String, description: String, absoluteSocialImage: String, absoluteURL: String, canonicalURL: String) {
         self.title = title
         self.description = description
-        self.keywords = keywords
         self.absoluteSocialImage = absoluteSocialImage
         self.absoluteURL = absoluteURL
         self.canonicalURL = canonicalURL
--- Sources/Pages/Articles/Meta/CommonMeta.swift
+++ Sources/Pages/Articles/Meta/CommonMeta.swift
@@ -11,22 +11,25 @@ import HTMLDSL
 func commonMeta(_ meta: MetaDetail, isApp: Bool = false) -> some HTMLHeadContentView {
     AnyView([
         AnyView(Title(meta.title)),
-        AnyView(htmlMeta(description: meta.description, keywords: meta.keywords, isApp: isApp)),
-        AnyView(socialMeta(title: meta.title,
-                           description: meta.description,
-                           image: meta.absoluteSocialImage,
-                           url: meta.absoluteURL))
+        AnyView(htmlMeta(description: meta.description, isApp: isApp)),
+        AnyView(
+            socialMeta(
+                title: meta.title,
+                description: meta.description,
+                image: meta.absoluteSocialImage,
+                url: meta.absoluteURL
+            )
+        )
     ])
 }
 
-private func htmlMeta(description: String, keywords: Set<String>, isApp: Bool) -> some HTMLHeadContentView {
+private func htmlMeta(description: String, isApp: Bool) -> some HTMLHeadContentView {
     let viewPort = isApp ? Meta(.viewport(width: .deviceWidth, scale: .full, maxScale: .full, isUserInteractionDisabled: true)) : Meta(.viewport(width: .deviceWidth, scale: .full))
     
     return AnyView([
         Meta(.characterSet(.utf8)),
         Meta(.name(.author, content: "Shahrukh Alam")),
         Meta(.name(.description, content: description)),
-        Meta(.name(.keywords, content: keywords.joined(separator: ", "))),
         viewPort
     ])
 }
