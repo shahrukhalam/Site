ebc1b0fb4a27d1de21662eb662d5957ec64f4d51
Shahrukh Alam <alam.shahrukh786@gmail.com>
2023-09-10 13:11:19 +0530

    Adds meta for indexing or not

--- Sources/Modeling/Common/MetaDetail.swift
+++ Sources/Modeling/Common/MetaDetail.swift
@@ -3,11 +3,13 @@ public struct MetaDetail {
     public let description: String
     public let absoluteSocialImage: String
     public let absoluteURL: String
+    public let isIndexable: Bool
 
-    public init(title: String, description: String, absoluteSocialImage: String, absoluteURL: String) {
+    public init(title: String, description: String, absoluteSocialImage: String, absoluteURL: String, isIndexable: Bool) {
         self.title = title
         self.description = description
         self.absoluteSocialImage = absoluteSocialImage
         self.absoluteURL = absoluteURL
+        self.isIndexable = isIndexable
     }
 }
--- Sources/Pages/Articles/Meta/CommonMeta.swift
+++ Sources/Pages/Articles/Meta/CommonMeta.swift
@@ -11,7 +11,7 @@ import HTMLDSL
 func commonMeta(_ meta: MetaDetail, isApp: Bool = false) -> some HTMLHeadContentView {
     AnyView([
         AnyView(Title(meta.title)),
-        AnyView(htmlMeta(description: meta.description, isApp: isApp)),
+        AnyView(htmlMeta(description: meta.description, isIndexable: meta.isIndexable, isApp: isApp)),
         AnyView(
             socialMeta(
                 title: meta.title,
@@ -23,14 +23,25 @@ func commonMeta(_ meta: MetaDetail, isApp: Bool = false) -> some HTMLHeadContent
     ])
 }
 
-private func htmlMeta(description: String, isApp: Bool) -> some HTMLHeadContentView {
+private func htmlMeta(description: String, isIndexable: Bool, isApp: Bool) -> some HTMLHeadContentView {
     let viewPort = isApp ? Meta(.viewport(width: .deviceWidth, scale: .full, maxScale: .full, isUserInteractionDisabled: true)) : Meta(.viewport(width: .deviceWidth, scale: .full))
     
-    return AnyView([
-        Meta(.characterSet(.utf8)),
-        // TODO: Fix by actual Author
-        Meta(.name(.author, content: "Shahrukh Alam")),
-        Meta(.name(.description, content: description)),
-        viewPort
-    ])
+    return isIndexable ? AnyView(
+        [
+            Meta(.characterSet(.utf8)),
+            // TODO: Fix by actual Author
+            Meta(.name(.author, content: "Shahrukh Alam")),
+            Meta(.name(.description, content: description)),
+            viewPort
+        ]
+    ) : AnyView(
+        [
+            Meta(.characterSet(.utf8)),
+            // TODO: Fix by actual Author
+            Meta(.name(.author, content: "Shahrukh Alam")),
+            Meta(.name(.description, content: description)),
+            viewPort,
+            Meta(.noindex)
+        ]
+    )
 }
