1078d105fb762ca01186b6e1dc782c7ea4cd4659
Shahrukh Alam <alam.shahrukh786@gmail.com>
2024-06-18 09:39:12 +0530

    prepares for notion article in swiftui

--- Sources/HelperApp/Models/Response/Response + JSON.swift
+++ Sources/HelperApp/Models/Response/Response + JSON.swift
@@ -8,4 +8,16 @@ public extension Response {
         response.body = .init(data: data)
         return response
     }
+
+    static func json(_ value: String) throws -> Response {
+        let response = Response()
+        response.headers.contentType = .json
+
+        guard let data = value.data(using: .utf8) else {
+            throw Abort(.partialContent, reason: "Failed to convert String to Data")
+        }
+
+        response.body = .init(data: data)
+        return response
+    }
 }
