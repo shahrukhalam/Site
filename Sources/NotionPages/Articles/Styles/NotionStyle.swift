a74b3bfcd892bbdec8a0d9b66a68ef424e938b8f
Shahrukh Alam <alam.shahrukh786@gmail.com>
2024-08-05 10:10:13 +0530

    Adds only iFrame related styles instead of specific to video

--- Sources/NotionPages/Articles/Styles/NotionStyle.swift
+++ Sources/NotionPages/Articles/Styles/NotionStyle.swift
@@ -4,12 +4,12 @@ import Pages
 struct NotionStyle: CSSStyle {
     let key: CustomStringConvertible
     let styles = [Style]()
-    
+
     var element: String
-    
+
     init(_ mediaType: MediaStyle.DeviceType) {
         self.key = Tag.empty.description
-        
+
         let styles: [CSSStyle]
         switch mediaType {
         case .wide:
@@ -19,26 +19,26 @@ struct NotionStyle: CSSStyle {
                 .lineHeight(.number(Typography.LineHeight.body))
                 .foregroundVariable(.index_foreground)
                 .margin(top: .length(.relativeToRoot(Typography.Margin.body)))
-            
+
             // MARK: Text
 
             let h1Style = ClassStyle(forClass: .notion(.page), withTag: .enclosing(.headings(.h1)))
                 .font(size: .relativeToRootFontSize(Typography.Font.Size.title))
                 .lineHeight(.number(Typography.LineHeight.title))
                 .margin(top: .length(.relativeToRoot(Typography.Margin.title)))
-            
+
             let h2Style = ClassStyle(forClass: .notion(.page), withTag: .enclosing(.headings(.h2)))
                 .font(size: .relativeToRootFontSize(Typography.Font.Size.heading1))
                 .font(weight: .number(600))
                 .lineHeight(.number(Typography.LineHeight.heading))
                 .margin(top: .length(.relativeToRoot(Typography.Margin.heading1)))
-            
+
             let h3Style = ClassStyle(forClass: .notion(.page), withTag: .enclosing(.headings(.h3)))
                 .font(size: .relativeToRootFontSize(Typography.Font.Size.heading2))
                 .font(weight: .number(600))
                 .lineHeight(.number(Typography.LineHeight.heading))
                 .margin(top: .length(.relativeToRoot(Typography.Margin.heading2)))
-            
+
             let h4Style = ClassStyle(forClass: .notion(.page), withTag: .enclosing(.headings(.h4)))
                 .font(size: .relativeToRootFontSize(Typography.Font.Size.heading3))
                 .font(weight: .number(600))
@@ -76,9 +76,9 @@ struct NotionStyle: CSSStyle {
                 )
                 .cornerRadius(uniform: .length(.relativeToRoot(Typography.Margin.subheading)))
 
-            // MARK: Video
+            // MARK: iFrame
 
-            let videoIframeContainerStyle = ClassStyle(forClass: .notion(.video_iframe_container))
+            let iframeContainerStyle = ClassStyle(forClass: .notion(.iframe_container))
                 .margin(
                     left: .auto,
                     top: .length(.relativeToRoot(Typography.Margin.heading1)),
@@ -94,7 +94,21 @@ struct NotionStyle: CSSStyle {
                 .size(width: .percentage(100))
                 .backgroundVariable(.note_background)
                 .cornerRadius(uniform: .pixel(10))
-            let videoIframeStyle = ClassStyle(forClass: .notion(.page), withClass: .notion(.video_iframe))
+
+            let iframeContainerStyleForPodcast = ClassStyle(forClass: .notion(.iframe_container_podcast))
+                .margin(
+                    top: .length(.relativeToRoot(Typography.Margin.heading1)),
+                    bottom: .length(.relativeToRoot(Typography.Margin.heading1))
+                )
+                .padding(
+                    top: .pixel(175)
+                )
+                .position(.relative)
+                .size(width: .percentage(100))
+                .backgroundVariable(.note_background)
+                .cornerRadius(uniform: .pixel(10))
+
+            let iframeStyle = ClassStyle(forClass: .notion(.page), withClass: .notion(.iframe))
                 .position(.absolute)
                 .constraint(left: .pixel(0), top: .pixel(0), right: .pixel(0), bottom: .pixel(0))
                 .sizeFull()
@@ -107,21 +121,22 @@ struct NotionStyle: CSSStyle {
                 h2Style,
                 h3Style,
                 h4Style,
+                iframeContainerStyle,
+                iframeContainerStyleForPodcast,
+                iframeStyle,
                 pStyle,
                 captionStyle,
                 linkStyle,
                 linkHoverStyle,
-                imageStyle,
-                videoIframeContainerStyle,
-                videoIframeStyle
+                imageStyle
             ]
         case .small:
             let mediaStyles: CSSStyle = [
             ]
-            
+
             styles = [MediaStyle(for: .small, with: mediaStyles)]
         }
-        
+
         self.element = styles.map { $0.element }.joined(separator: "\n")
     }
 }
